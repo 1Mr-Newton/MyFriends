838656869a3118b1bb87d44dc01ddb5ce822c832
1mr-newton <eaidoo015@gmail.com>
2022-08-24 17:45:39 +0000

    commit

--- app.py
+++ app.py
@@ -3,23 +3,28 @@ from flask import Flask, render_template, redirect, request, url_for, session
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST", "GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-  return render_template('home.html')
+    return render_template("index.html")
 
 
+@app.route("/user/<name>")
+def user(name):
+    user_name = name
+
+    return render_template("user.html", user_name=user_name)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
-def page_not_foun(e):
-  return render_template('500.html', 500)
+def page_not_found(e):
+    return render_template("500.html"), 500
 
 
-
-
-if __name__ == "__main__":app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
