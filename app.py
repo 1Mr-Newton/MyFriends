9fc24a0ac1284423301c78337fd7591581281f5c
1mr-newton <eaidoo015@gmail.com>
2022-08-24 18:06:35 +0000

    commit

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, redirect, request, url_for, session
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my secret key that is very difficult for hackers to hack"
+
+# create form class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("name.html", name=name, form=form)
 
 
 @app.route("/", methods=["POST", "GET"])
