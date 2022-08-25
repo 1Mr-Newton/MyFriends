3870d5d8a5a84e3d865e1f21779fcd00bd9f98a2
1mr-newton <eaidoo015@gmail.com>
2022-08-25 07:59:37 +0000

    commit

--- app.py
+++ app.py
@@ -1,17 +1,59 @@
+import email
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "my secret key that is very difficult for hackers to hack"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://root:password123@localhost/our_users"
 
+db = SQLAlchemy(app)
 # create form class
 class NamerForm(FlaskForm):
-    name = StringField("What's Your Name", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+# create db model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # create a return string
+    def __repr__(self):
+        return "<Name %r>" % self.name
+
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        # print(user)
+        # print(form.email.data)
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("User Added Successfully")
+        else:
+            flash("Email Already Exists!")
+
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
 
 
 @app.route("/name", methods=["GET", "POST"])
