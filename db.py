103df9a910c6a6eaf6cc0f5b9b8ba9aa7b939fd0
1mr-newton <eaidoo015@gmail.com>
2022-08-27 08:20:42 +0000

    commit

--- db.py
+++ db.py
@@ -9,11 +9,11 @@
 
 
 # create db with mysql
-# import mysql.connector
+import mysql.connector
 
-# mydb = mysql.connector.connect(host="localhost", user="root", passwd="password123")
-# my_cursor = mydb.cursor()
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="password123")
+my_cursor = mydb.cursor()
 # my_cursor.execute("CREATE DATABASE our_users")
-# my_cursor.execute("SHOW DATABASES")
-# for db in my_cursor:
-#   print(db)
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+  print(db)
